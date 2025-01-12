c953703c49f6305e30ee8fe51ac718a1d30918ff
Marco Tulio <marcotfrancosilva@gmail.com>
2025-01-12 15:25:25 -0300

    Consertando as inserções das instancias padrao TSPlib

--- main.cpp
+++ main.cpp
@@ -1,43 +1,107 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <sstream>
+#include <string>
+#include <vector>
 #include <map>
 #include <fstream>
 
 using namespace std;
 
-// TSP- Insercao Mais Proxima
+const char delimiter = ':'; // Delimitador entre a palavra-chave e o valor
 
-int main()
+struct TSPProblem
+{
+    string type;
+    int dimension;
+    string edgeWeightType;
+    string displayDataType;
+    map<int, pair<double, double>> node;
+};
+
+bool checkKeyword(string keyword, string value, TSPProblem &problem)
+{
+    if (keyword == "TYPE")
+    {
+        if (value == "TSP" || value == "ATSP")
+        {
+            problem.type = value;
+        }
+        else
+        {
+            cout << keyword << " não suportado." << endl;
+            return false;
+        }
+    }
+    else if (keyword == "DIMENSION")
+    {
+        problem.dimension = stoi(value);
+    }
+    else if (keyword == "EDGE_WEIGHT_TYPE")
+    {
+        if (value == "GEO")
+        {
+            problem.edgeWeightType = value;
+        }
+        else if (value == "EUC_2D")
+        {
+            problem.edgeWeightType = value;
+        }
+        else
+        {
+            cout << keyword << " não suportado." << endl;
+            return false;
+        }
+    }
+    else if (keyword == "DISPLAY_DATA_TYPE")
+    {
+        if (value == "COORD_DISPLAY")
+        {
+            problem.displayDataType = value;
+        }
+        else
+        {
+            cout << keyword << " não suportado." << endl;
+            return false;
+        }
+    }
+    else
+    {
+        cout << "Erro: Palavra-chave desconhecida " << keyword << endl;
+        return false;
+    }
+    return true;
+}
+
+string trim(string s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char c)
+                         { return !std::isspace(c); })
+                .base(),
+            s.end());
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char c)
+                                    { return !std::isspace(c); }));
+    return s;
+}
+
+void readInstance(TSPProblem &problem)
 {
     string line;
-    string type, edge_weight_type, display_data_type;
-    int dimension;
-    map<int, pair<double, double>> nodes;
-    map<int, vector<pair<int, double>>> adjacencyList; // Lista de adjacência
-
     while (getline(cin, line))
     {
-        if (line.find("TYPE:") != string::npos)
-        {
-            type = line.substr(line.find(":") + 2);
-        }
-        else if (line.find("DIMENSION:") != string::npos)
-        {
-            dimension = stoi(line.substr(line.find(":") + 2));
-        }
-        else if (line.find("EDGE_WEIGHT_TYPE:") != string::npos)
-        {
-            edge_weight_type = line.substr(line.find(":") + 2);
-        }
-        else if (line.find("DISPLAY_DATA_TYPE:") != string::npos)
-        {
-            display_data_type = line.substr(line.find(":") + 2);
-        }
-        else if (line == "NODE_COORD_SECTION")
+        if (line == "NODE_COORD_SECTION")
         {
             break;
+        }
+        if (line.find(delimiter) != std::string::npos)
+        {
+            string keyword = line.substr(0, line.find(delimiter));
+            string value = line.substr(line.find(delimiter) + 1, line.npos);
+
+            if (!checkKeyword(trim(keyword), trim(value), problem))
+            {
+                break;
+            }
         }
     }
 
@@ -49,21 +113,29 @@ int main()
         }
         stringstream ss(line);
         int id;
-        double latitude, longitude;
-        ss >> id >> latitude >> longitude;
-        nodes[id] = {latitude, longitude};
+        double x, y;
+        ss >> id >> x >> y;
+        problem.node[id] = make_pair(x, y);
     }
+}
 
-    cout << "Lendo os dados:\n";
-    cout << "TYPE: " << type << endl;
-    cout << "DIMENSION: " << dimension << endl;
-    cout << "EDGE_WEIGHT_TYPE: " << edge_weight_type << endl;
-    cout << "DISPLAY_DATA_TYPE: " << display_data_type << endl;
-
-    for (const auto &node : nodes)
+void printInstance(TSPProblem &problem)
+{
+    cout << "TYPE: " << problem.type << endl;
+    cout << "DIMENSION: " << problem.dimension << endl;
+    cout << "EDGE_WEIGHT_TYPE: " << problem.edgeWeightType << endl;
+    cout << "DISPLAY_DATA_TYPE: " << problem.displayDataType << endl;
+    cout << "NODE_COORD_SECTION" << endl;
+    for (auto &p : problem.node)
     {
-        cout << node.first << " " << node.second.first << " " << node.second.second << endl;
+        cout << p.first << " " << p.second.first << " " << p.second.second << endl;
     }
+}
 
+int main()
+{
+    TSPProblem problem;
+    readInstance(problem);
+    printInstance(problem);
     return 0;
 }
